e0def5f0b7a7cee7a6b330638228046351b5e865
your-username <your-email>
2025-08-01 15:30:39 -0700

    Removed a comment

--- loan.py
+++ loan.py
@@ -1,4 +1,3 @@
-# Get details of loan
 money_owed = float(input('How much money do you owe, in dollars?\n'))
 apr = float(input('What is the annual percentage rate of the loan?\n'))
 payment = float(input('How much will ou pay off each month in dollars?\n'))
